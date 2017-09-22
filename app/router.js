e01454b382c15925b5547b9925ba9747926f27ce
scheerp <patrick-scheer@web.de>
2017-09-22 18:07:08 +0200

    almost done

--- app/router.js
+++ app/router.js
@@ -8,9 +8,9 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('about');
-  this.route('contact');
   this.route('portfolio');
   this.route('project', { path: '/:id' });
+  this.route('loading');
 });
 
 export default Router;
