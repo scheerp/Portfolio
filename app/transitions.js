5df3e73383eab5ab9e1f6dfcda6df2792119d4aa
scheerp <patrick-scheer@web.de>
2017-09-23 22:36:14 +0200

    added lazy picture loading

--- app/transitions.js
+++ app/transitions.js
@@ -1,136 +1,17 @@
 export default function() {
   const duration = 300;
 
-  // Toplevel transiotions+++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   this.transition(
-    this.fromRoute('contact'),
-    this.toRoute('about'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-  this.transition(
-    this.fromRoute('index.contact'),
-    this.toRoute('index.about'),
-    this.use('crossFade'),
-    this.reverse('crossFade')
+    this.hasClass('overlay'),
+    this.toValue(true),
+    this.use('to-up'),
+    this.reverse('fade')
   );
 
   this.transition(
-    this.fromRoute('about'),
-    this.toRoute('portfolio'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-  this.transition(
-    this.fromRoute('index.about'),
-    this.toRoute('index.portfilio'),
-    this.use('crossFade'),
-    this.reverse('crossFade')
-  );
-
-  this.transition(
-    this.fromRoute('contact'),
-    this.toRoute('portfolio'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-  this.transition(
-    this.fromRoute('index.contact'),
-    this.toRoute('index.portfolio'),
-    this.use('crossFade'),
-    this.reverse('crossFade')
-  );
-
-  this.transition(
-    this.fromRoute('rocketbeans'),
-    this.toRoute('contact'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-  this.transition(
-    this.fromRoute('index.rocketbeans'),
-    this.toRoute('index.contact'),
-    this.use('crossFade'),
-    this.reverse('crossFade')
-  );
-
-  this.transition(
-    this.fromRoute('rocketbeans'),
-    this.toRoute('about'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-  this.transition(
-    this.fromRoute('index.rocketbeans'),
-    this.toRoute('index.about'),
-    this.use('crossFade'),
-    this.reverse('crossFade')
-  );
-
-  this.transition(
-    this.fromRoute('rocketbeans'),
-    this.toRoute('portfolio'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-  this.transition(
-    this.fromRoute('index.rocketbeans'),
-    this.toRoute('index.portfolio'),
-    this.use('crossFade'),
-    this.reverse('crossFade')
-  );
-
-  this.transition(
-    this.fromRoute('portfolio'),
-    this.toRoute('project'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-  this.transition(
-    this.fromRoute('index.portfolio'),
-    this.toRoute('index.project'),
-    this.use('crossFade'),
-    this.reverse('crossFade')
-  );
-
-  this.transition(
-    this.fromRoute('rocketbeans'),
-    this.toRoute('project'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-  this.transition(
-    this.fromRoute('index.rocketbeans'),
-    this.toRoute('index.project'),
-    this.use('crossFade'),
-    this.reverse('crossFade')
-  );
-
-  this.transition(
-    this.fromRoute('about'),
-    this.toRoute('project'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-  this.transition(
-    this.fromRoute('index.about'),
-    this.toRoute('index.project'),
-    this.use('crossFade'),
-    this.reverse('crossFade')
-  );
-
-  this.transition(
-    this.fromRoute('contact'),
-    this.toRoute('project'),
-    this.use('toRight'),
-    this.reverse('toLeft')
-  );
-  this.transition(
-    this.fromRoute('index.contact'),
-    this.toRoute('index.project'),
-    this.use('crossFade'),
-    this.reverse('crossFade')
+    this.hasClass('hello-world'),
+    this.toValue(true),
+    this.use('to-down'),
+    this.reverse('to-right')
   );
 }
-
-// Project-transition++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
